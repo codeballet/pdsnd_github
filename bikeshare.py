b44c251ed9b8e4e601ff6af1f684c3242eff087a
Johan Stjernholm <stjernholm@msn.com>
2019-03-09 00:05:06 +0800

    refactor: moved .lower() in main() function to input()

--- bikeshare.py
+++ bikeshare.py
@@ -387,8 +387,8 @@ def main():
 
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = input('\nWould you like to restart? Enter yes or no.\n').lower()
+        if restart != 'yes':
             break
 
 
